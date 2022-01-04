ac06b2a90a21181c99f1277189ca4dc7ed974be7
Hal <hgrforrest@gmail.com>
2022-01-04 14:23:20 -0500

    testing streams for file reading

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-	fs.readFile(__dirname + '/data.txt', (err, data) => {
-		console.log(data.toString())
-		res.end(data)
-	})
+	// fs.readFile(__dirname + '/data.txt', (err, data) => {
+	// 	console.log(data.toString())
+	// 	res.end(data)
+	// })
+	const stream = fs.createReadStream(__dirname + '/data.txt')
+	stream.pipe(res)
 })
 server.listen(3000)
+
